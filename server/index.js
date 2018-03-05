bb3ce7409af8c251f57086f54b160bdb75e3d9f5
Olya <olga.akinshyna@gmail.com>
2018-03-05 11:16:47 +0200

    error handling

--- server/index.js
+++ server/index.js
@@ -8,8 +8,14 @@ server.use('/api/cars', routes)
 server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Content-Type', 'application/json')
     next();
 });
+
+server.use(function(err, req, res, nex) {
+    console.error(err.message)
+    res.status(err.statusCode).send(err.message)
+})
 
 const PORT = Math.round(Math.random() * 1000)
 
