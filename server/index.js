113422dd1cd422aa036ef0eae828362f10f6ff59
Olya <olga.akinshyna@gmail.com>
2018-03-05 17:51:10 +0200

    server index

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ const routes = require('./routes/routes.js')
 
 const server = express()
 
-server.use('/api/cars', routes)
-
 server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -12,12 +10,14 @@ server.use(function(req, res, next) {
     next();
 });
 
+server.use('/api/cars', routes)
+
 server.use(function(err, req, res, nex) {
     console.error(err.message)
     res.status(err.statusCode).send(err.message)
 })
 
-const PORT = Math.round(Math.random() * 1000)
+const PORT = 6650 //Math.round(Math.random() * 1000)
 
 server.listen(PORT, () => {
     console.log(`Listeneing to port: ${PORT}.`)
